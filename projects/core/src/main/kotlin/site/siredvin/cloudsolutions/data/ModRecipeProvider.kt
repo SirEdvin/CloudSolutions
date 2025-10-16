57427efbce5bc0ef4ff7b4c800acfe83a780258b
SirEdvin <me@siredvin.site>
2025-10-17 00:50:38 +0300

    feat(crafka_broker): add recipe

--- projects/core/src/main/kotlin/site/siredvin/cloudsolutions/data/ModRecipeProvider.kt
+++ projects/core/src/main/kotlin/site/siredvin/cloudsolutions/data/ModRecipeProvider.kt
@@ -4,6 +4,7 @@ import net.minecraft.data.PackOutput
 import net.minecraft.data.recipes.FinishedRecipe
 import net.minecraft.data.recipes.RecipeProvider
 import net.minecraft.tags.ItemTags
+import net.minecraft.world.item.Items
 import site.siredvin.broccolium.modules.data.recipe.TweakedShapedRecipeBuilder
 import site.siredvin.cloudsolutions.common.setup.ModBlocks
 import site.siredvin.cloudsolutions.xplat.ModRecipeIngredients
@@ -27,6 +28,15 @@ class ModRecipeProvider(output: PackOutput) : RecipeProvider(output) {
             .pattern("CMC")
             .pattern("BIB")
             .pattern("CBC")
+            .save(consumer)
+        TweakedShapedRecipeBuilder.shaped(ModBlocks.CRAFKA_BROKER.get())
+            .define('I', ModRecipeIngredients.get().ironBlock)
+            .define('C', Items.LIGHTNING_ROD)
+            .define('R', Items.IRON_BARS)
+            .define('D', ItemTags.WOOL)
+            .pattern("DCD")
+            .pattern("RIR")
+            .pattern("RDR")
             .save(consumer)
     }
 }
