20c682a32e70d94c92ac2126800d5be076dfe4c9
SirEdvin <me@siredvin.site>
2025-10-17 00:28:37 +0300

    style: fix

--- projects/core/src/main/kotlin/site/siredvin/cloudsolutions/subsystems/tsdb/dummy/TSDBDummyManager.kt
+++ projects/core/src/main/kotlin/site/siredvin/cloudsolutions/subsystems/tsdb/dummy/TSDBDummyManager.kt
@@ -17,9 +17,7 @@ object TSDBDummyManager : TSDBManager {
         tags: Map<String, String>,
         from: Instant,
         to: Instant,
-    ): List<TimeseriesFrame> {
-        return emptyList()
-    }
+    ): List<TimeseriesFrame> = emptyList()
 
     override fun getOrCreate(ownerUUID: String, name: String, tags: Map<String, String>, retention: Int): UUID = throw NotImplementedError("You shound't use dummy manager for this")
 
