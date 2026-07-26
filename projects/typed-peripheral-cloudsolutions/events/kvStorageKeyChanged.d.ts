601f166d06b647cb3c33dddb2e64f920a1d1ace8
SirEdvin <me@siredvin.site>
2026-07-26 10:41:43 +0000

    chore: ignore generated TypeScript declarations

--- projects/typed-peripheral-cloudsolutions/events/kvStorageKeyChanged.d.ts
+++
@@ -1,9 +0,0 @@
-import * as events from "@siredvin/cc-events";
-export declare const KV_STORAGE_KEY_CHANGED = "kv_storage_key_changed";
-export declare class KVStorageKeyChangedEvent extends events.BaseEvent<[
-    string,
-    string
-]> {
-    getKey(): string;
-    getValue(): string;
-}
